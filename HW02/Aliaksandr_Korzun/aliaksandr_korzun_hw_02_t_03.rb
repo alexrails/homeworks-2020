a8633d49f99990b6a35082c5d2055dc7c4497403
Alex <alexrails@yandex.ru>
2020-03-22 18:11:14 +0300

    Add duration_of_actions method

---
+++ HW02/Aliaksandr_Korzun/aliaksandr_korzun_hw_02_t_03.rb
@@ -0,0 +1,47 @@
+require 'time'
+
+logs = 'logfile_task_3.txt'
+
+SUBSTRING = 'calling core'
+TIME_TEMPLATE = /^\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\:\d{2}\.\d{1}/
+
+def duration_of_actions(logs)
+  result_hash = {}
+
+  #Create array of logs
+  file_data = File.readlines(logs).map(&:chomp).
+    delete_if { |line| core?(line) }
+
+  file_data.each.with_index do |line, index|
+    if index < file_data.size - 1
+      first_action = last_word(line)
+      start = parse_time(line)
+
+      next_line = file_data[index + 1]
+      second_action = last_word(next_line)
+      finish = parse_time(next_line)
+
+      result_hash["#{first_action}->#{second_action}"] = "#{finish - start}"
+    end
+  end
+  p result_hash
+end
+
+private
+
+#Find substring
+def core?(line)
+  !line.downcase.include?(SUBSTRING)
+end
+
+#Parsing time
+def parse_time(line)
+  Time.parse(line[TIME_TEMPLATE])
+end
+
+#Find last word to string
+def last_word(line)
+  line.split.last
+end
+
+duration_of_actions(logs)
