7a74b885feeadebc76b63687c5561ce26a5f813e
Alex <alexrails@yandex.ru>
2020-03-25 23:57:13 +0300

    Change Regexp

--- HW02/Aliaksandr_Korzun/aliaksandr_korzun_hw_02_t_02.rb
+++ HW02/Aliaksandr_Korzun/aliaksandr_korzun_hw_02_t_02.rb
@@ -2,17 +2,21 @@
 
 LOG_FILE = 'logfile.txt'
 
-IP_ADDRESS = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.freeze
-DATE_INFO = /\[.*\]/.freeze
-PATH = /\".*\"/.freeze
+IP = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.freeze
+DATE = %r{\[\d{1,2}/[a-zA-z]{3}/\d{4}\:\d{2}\:\d{2}\:\d{2}\s\+\d{4}\]}.freeze
+PATH = %r{\"\w+\s/\w+/\d+/\w+}.freeze
+ANSWER = /\d{3}\s\d+\s\d+\.\d+/.freeze
 
+FULL_LOG = %r{#{IP}\s\-\s\-\s#{DATE}\s#{PATH}\s\w+/\d+\.\d+\"\s#{ANSWER}}.freeze
 def format_log_string(logs)
   file_data = File.readlines(logs).map(&:chomp)
                   .delete_if { |line| error?(line) }
 
   file_data.map do |line|
-    (line.slice(DATE_INFO)[1..-2]).to_s + ' FROM ' \
-    "#{line.slice(IP_ADDRESS)} " \
+    next unless line.match?(FULL_LOG)
+
+    (line.slice(DATE)[1..-2]).to_s + ' FROM ' \
+    "#{line.slice(IP)} " \
     "#{line.slice(PATH).gsub('POST', 'TO')[1..-2]}"
   end
 end
