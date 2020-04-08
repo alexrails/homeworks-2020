a1d96cf1b2e2824cf575bef812fde69a924c6896
Alex <alexrails@yandex.ru>
2020-04-09 02:54:22 +0300

    Fix rubocop errors

--- HW04/Aliaksandr_Korzun/array.rb
+++ HW04/Aliaksandr_Korzun/array.rb
@@ -1,26 +1,24 @@
 class Array
-
-def my_each
-  index = 0
-  while index < self.length
-    yield self[index]
-    index += 1
+  def my_each
+    index = 0
+    while index < length
+      yield self[index]
+      index += 1
+    end
+    self
   end
-  self
-end
 
-def my_map
-  return_array = []
-  self.my_each{ |element| return_array << yield(element) }
-  return_array
-end
-
-def my_select
-  return_array = []
-  self.my_each do |element|
-    return_array << element if yield(element) == true
+  def my_map
+    return_array = []
+    my_each { |element| return_array << yield(element) }
+    return_array
   end
-  return_array
-end
 
+  def my_select
+    return_array = []
+    my_each do |element|
+      return_array << element if yield(element) == true
+    end
+    return_array
+  end
 end
---
+++ HW04/Aliaksandr_Korzun/spec/array_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../array.rb'
+
+RSpec.describe Array do
+  subject { described_class.new(%w[a b c]) }
+
+  describe '#my_each' do
+    it 'return source array' do
+      expect(subject.my_each { |element| element * 2 }).to eql(subject)
+    end
+  end
+
+  describe '#my_map' do
+    it 'return new array' do
+      expect(subject.my_map { |element| element * 2 }).to eql(%w[aa bb cc])
+    end
+  end
+
+  describe '#my_select' do
+    it 'return an array satisfying the condition' do
+      expect(subject.my_select { |element| element == 'a' }).to eql(['a'])
+    end
+  end
+end
